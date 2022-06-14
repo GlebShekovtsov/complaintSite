b3cf13b7faeb44e1f91208ce4e4c2e53d268951c
GlebShekovtsov <80450328+GlebShekovtsov@users.noreply.github.com>
2022-06-14 18:50:51 +0300

    Add files via upload

---
+++ index.php
@@ -0,0 +1,72 @@
+<!DOCTYPE html>
+<html lang="ru">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/normalize.css">
+    <link rel="stylesheet" href="css/style.css">
+    <title>Document</title>
+</head>
+
+<body>
+    <div class="container">
+        <div class="topline__menu">
+
+            <div class="topline__asidemenu">
+                <input type="checkbox" id="hmt" class="hidden-menu-ticker">
+                <label class="btn-menu" for="hmt">
+                    <span class="first"></span>
+                    <span class="second"></span>
+                    <span class="third"></span>
+                </label>
+                <div class="hidden-menu">
+                    <div class="side__content">
+                        <h2 class="side__title">
+                            Официальный сетевой портал
+                        </h2>
+                        <h3 class="side__caption">
+                            Президента Арстотцки
+                        </h3>
+                        <div class="side__menu">
+                            <a href="#" class="side__items">item</a>
+                            <a href="#" class="side__items">item</a>
+                            <a href="#" class="side__items">item</a>
+                            <a href="#" class="side__items">item</a>
+                        </div>
+                        <div class="side__footer">
+                            <p class="side__paragraph">
+                                Администрация Президента Арстотцки
+                            </p>
+                            <p class="side__paragraph">
+                                2133 год
+                            </p>
+                        </div>
+
+                    </div>
+
+                </div>
+            </div>
+
+            <div class="topline__in">
+                
+            </div>
+        </div>
+        <div class="top__nav">
+
+        </div>
+        <div class="main__content">
+
+            <div class="inner__content">
+
+
+
+            </div>
+
+        </div>
+    </div>
+
+</body>
+
+</html>
